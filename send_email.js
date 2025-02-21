6ffce7f25b3cd983da011f66f8a42a62c653038e
wald <manuwald16@gmail.com>
2025-02-21 18:46:54 -0300

    Updated email template and configuration for sending emails via Gmail

--- send_email.js
+++ send_email.js
@@ -1,72 +1,28 @@
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 
-// Leer el archivo HTML
 const emailHtml = fs.readFileSync("email.html", "utf8");
 
-// Configuración del transportador con Mailtrap SMTP
 let transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+    service: "gmail",
     auth: {
-        user: "35d7f00bb69b06",
-        pass: "0e9b90a3f2da0a"
+        user: "manuwald16@gmail.com",
+        pass: "ucgfhkqhsoufqvfx"
     }
 });
 
 let mailOptions = {
     from: '"Manuel" <manuwald16@gmail.com>',
-    to: "test@example.com",
-    subject: "Test Email from Mailtrap",
-    html: emailHtml,
-    attachments: [
-        {
-            filename: "Logo_Payway_mosca.png",
-            path: "./Logo_Payway_mosca.png",
-            cid: "logo_payway" // El mismo cid usado en el HTML
-        },
-        {
-            filename: "Logo_Payway_con_Claim.png",
-            path: "./Logo_Payway_con_Claim.png",
-            cid: "logo_Claim"
-        },
-        {
-            filename: "Tasa0_porcentaje.png",
-            path: "./Tasa0_porcentaje.png",
-            cid: "tasa0"
-        },
-        {
-            filename: "Payway_URL.png",
-            path: "./Payway_URL.png",
-            cid: "payway_url"
-        },
-        {
-            filename: "Wahtsapp_icono.png",
-            path: "./Wahtsapp_icono.png",
-            cid: "whatsapp_icon"
-        },
-        {
-            filename: "instagram_icono.png",
-            path: "./instagram_icono.png",
-            cid: "instagram_icon"
-        },
-        {
-            filename: "YouTube_icono.png",
-            path: "./YouTube_icono.png",
-            cid: "youtube_icon"
-        },
-        {
-            filename: "LInkeding_icono.png",
-            path: "./LInkeding_icono.png",
-            cid: "linkedin_icon"
-        }
-    ]
+    to: "javi.cerruti@gmail.com",
+    subject: "HTML de Manu",
+    html: emailHtml
 };
 
-// Enviar el correo
 transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-        return console.log(error);
+        return console.log("Error sending email:", error);
     }
-    console.log("Message sent: %s", info.messageId);
+    console.log("Email sent: %s", info.response);
 });
+
+
